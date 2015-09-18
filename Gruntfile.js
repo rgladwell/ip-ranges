55e47af7361008e80fd921281f936d518d958107
Ricardo Gladwell <ricardo@gladwell.me>
2015-09-18 11:18:46 +0100

    Copied Digital Ocean template project from assets.
    Copied template Node.js project from assets project:
    
    https://github.com/is-hosted-by/is-hosted-by-assets
    
    This project just contains IP ranges for Digital Ocean network as
    modified from the source here:
    
    https://github.com/hanswolff/ipranges/blob/master/IpRanges/Resources/DigitalOcean.xml

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {
+            src: 'index.html',
+            dest: 'dist/index.html'
+          },
+          {
+            src: 'CNAME',
+            dest: 'dist/CNAME'
+          }
+        ]
+      },
+      assets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'assets/',
+            src: ['**', '!styles/**'],
+            dest: 'dist/'
+          }
+        ],
+      },
+      bower_components: {
+        files: [
+          {
+            expand: true,
+            cwd: 'bower_components/',
+            src: ['**'],
+            dest: 'dist/vendor/'
+          }
+        ],
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      html: {
+        files: ['index.html'],
+        tasks: ['copy:main']
+      },
+      bower_components: {
+        files: ['bower_components/**'],
+        tasks: ['copy:bower_components']
+      }
+    },
+
+    clean: {
+      release: ["dist"]
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: 'dist',
+          hostname: 'localhost',
+          livereload: true
+        }
+      }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:is-hosted-by/ip-ranges.git',
+          branch: 'gh-pages'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-build-control');
+
+  grunt.registerTask('default', ['copy']);
+  grunt.registerTask('run', ['clean', 'default', 'connect', 'watch']);
+  grunt.registerTask('deploy', ['clean', 'default', 'buildcontrol']);
+};
---
+++ README.md
@@ -0,0 +1,31 @@
+#IP Ranges
+
+Lists of IP ranges for popular hosting providers, based on (Hans Wolff's)[https://github.com/hanswolff] excellent [ipranges](https://github.com/hanswolff/ipranges) library.
+
+**Note:** THERE IS NO GUARANTEE THAT THE PROVIDED IP RANGES ARE ACCURATE! PLEASE READ THE "LICENSE" FILE!
+
+Requirements:
+
+  * [Node.js](http://nodejs.org/).
+
+To set-up your environment execute:
+
+    $ npm install
+
+To build execute:
+
+    $ npm run build
+
+To test:
+
+    $ npm run server
+
+And open http://localhost:8000 in your browser.
+
+To clean build:
+
+    $ npm run clean
+
+To deploy, [set-up a Divshot](http://docs.divshot.com/guides/getting-started) and run:
+
+    $ npm run deploy
